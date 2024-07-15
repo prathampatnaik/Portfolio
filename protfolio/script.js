d601085b9bd33496a0c494a498b44d91ca47a964
pratham patnaik <108348807+prathampatnaik@users.noreply.github.com>
2024-07-15 16:50:32 +0530

    Add files via upload

---
+++ protfolio/script.js
@@ -0,0 +1,7 @@
+function togglemenu(){
+    const menu = document.querySelector(".menu-link");
+    const icon = document.querySelector(".ham-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    
+}
